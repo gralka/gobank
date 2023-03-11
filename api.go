708462d13f3b9b93e8e1d3a15b0a2b36304666af
J.T. Gralka <contact@jtgralka.com>
2023-03-10 23:37:30 -0500

    chore: move api helper funcs to the bottom
    Also rename the "accounts" endpoint to pluralize it.

--- api.go
+++ api.go
@@ -9,27 +9,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, value any) error {
-  w.WriteHeader(status)
-  w.Header().Set("Content-Type", "application/json")
-
-  return json.NewEncoder(w).Encode(value)
-}
-
-type apiFunc func (http.ResponseWriter, *http.Request) error
-
-type ApiError struct {
-  Error string
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-  return func (w http.ResponseWriter, r *http.Request) {
-    if err := f(w, r); err != nil {
-      WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-    }
-  }
-}
-
 type APIServer struct {
   listenAddr string
 }
@@ -43,8 +22,8 @@ func NewAPIServer(listenAddr string) *APIServer {
 func (s *APIServer) Run() {
   router := mux.NewRouter()
 
-  router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleAccount));
-  router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount));
+  router.HandleFunc("/accounts/{id}", makeHTTPHandleFunc(s.handleAccount));
+  router.HandleFunc("/accounts", makeHTTPHandleFunc(s.handleAccount));
 
   log.Println("JSON API Service Running on port: ", s.listenAddr)
 
@@ -87,3 +66,25 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
   return nil
 }
+
+func WriteJSON(w http.ResponseWriter, status int, value any) error {
+  w.Header().Add("Content-Type", "application/json")
+  w.WriteHeader(status)
+
+  return json.NewEncoder(w).Encode(value)
+}
+
+type apiFunc func (http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+  Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+  return func (w http.ResponseWriter, r *http.Request) {
+    if err := f(w, r); err != nil {
+      WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+    }
+  }
+}
+
